634cbb1c0feb7ba98ff6946c382c9d1af5f27736
Qwuenzy <117026518+Qwuenzy@users.noreply.github.com>
2025-11-05 18:18:38 +0100

    Corrections and little update
    the generate button will be disabled until a ticker is entered. Also your tickers will be here text will be replaced with the input the user typed. The code is more structure and concise in a way.

--- index.js
+++ index.js
@@ -6,18 +6,27 @@ const genrep = document.querySelector(".genrep");
 const report = document.querySelector(".report")
 const maincont = document.querySelector(".main-cont")
 
-const btnAdd = document.querySelector(".add");
 
-btnAdd.addEventListener("click", function add() {
-  inputP.textContent += inputText.value + ",";
-  inputText.value = "";
-});
-inputText.addEventListener("keydown", (event) => {
-    if (event.key === "Enter" && inputText.value.trim() !== "") {
-        inputP.textContent += inputText.value + ",";
-        inputText.value = "";
+window.addEventListener("load",() => {
+  inputP.textContent = "Your tickers will appear here..."
+  genrep.disabled = true
+})
+
+const btnAdd = document.querySelector(".add")
+function addBtn(){
+  if ( inputText.value.trim() !== "") {
+    if (inputP.textContent === "Your tickers will appear here...") {
+      inputP.textContent = "";
     }
-  });
+        inputP.textContent += inputText.value.toUpperCase() + ",";
+        inputText.value = "";
+        genrep.disabled = false
+    }
+}
+btnAdd.addEventListener("click", addBtn);
+inputText.addEventListener("keydown", (event) => event.key === "Enter" && addBtn());
+
+inputText.value.trim() !== "" ? btnAdd.disabled = true : btnAdd.disabled = false
 
 const spin = document.createElement("img");
 spin.src = "images/Iphone-spinner-2.gif";
@@ -30,10 +39,12 @@ report.style.display="none"
 genrep.addEventListener("click", () => {
   spin.style.display = "block";
   maincont.style.display= "none"
+  foot.textContent = "Loading..."
 
   setTimeout(() => {
     spin.style.display = "none";
     maincont.style.display= "none"
     report.style.display = "block"
+    foot.textContent = "Warning: This is not real financial advice!!!"
   }, 4000);
 });
