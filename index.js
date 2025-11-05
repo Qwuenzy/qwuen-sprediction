7c2341107463f02a84aad1207160045b1e1b42c9
Qwuenzy <117026518+Qwuenzy@users.noreply.github.com>
2025-11-05 14:21:26 +0100

    update the index.html file
    I worked on adding the spinner gif for the loading part and also showing the message part the output will return after some seconds.

--- index.js
+++ index.js
@@ -3,28 +3,37 @@ const inputP = document.querySelector(".input-para");
 const cont = document.querySelector(".cont");
 const foot = document.querySelector(".foot");
 const genrep = document.querySelector(".genrep");
+const report = document.querySelector(".report")
+const maincont = document.querySelector(".main-cont")
 
 const btnAdd = document.querySelector(".add");
 
-btnAdd.addEventListener("click", () => {
+btnAdd.addEventListener("click", function add() {
   inputP.textContent += inputText.value + ",";
   inputText.value = "";
 });
+inputText.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" && inputText.value.trim() !== "") {
+        inputP.textContent += inputText.value + ",";
+        inputText.value = "";
+    }
+  });
 
 const spin = document.createElement("img");
-  spin.src = "images/Iphone-spinner-2.gif";
-  spin.alt = "loading image";
-  cont.appendChild(spin)
-  spin.style.display = "none"
+spin.src = "images/Iphone-spinner-2.gif";
+spin.alt = "loading image";
+spin.classList.add("spinn")
+cont.appendChild(spin);
+spin.style.display = "none";
+report.style.display="none"
 
 genrep.addEventListener("click", () => {
-    
-    setTimeout(() => {
-    spin.style.display = "block"
-    cont.innerHTML = spin
-    foot.style.display = "none"
-  }, 1000)
-  cont.style.display = "block"
-    
-  
+  spin.style.display = "block";
+  maincont.style.display= "none"
+
+  setTimeout(() => {
+    spin.style.display = "none";
+    maincont.style.display= "none"
+    report.style.display = "block"
+  }, 4000);
 });
